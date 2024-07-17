cad8600c9553f790100e733e9d4d5cf395449eb0
Francisco Cavalcante <diegocavalcante.ti@gmail.com>
2024-07-17 19:56:50 +0000

    implementamos mais idiomas.

--- hello.py
+++ hello.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3 
+"""Hello World Multi Linguas.
 
-print ("hello, world!")
-#comentario desnecessario
+dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+como usar :
+
+tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+execução:
+    python3 hello.py
+	ou
+	./hello.py
+"""
+__version__ ="0.0.1"	
+__author__ = "Francisco Cavalcante"
+__licence__ = "unlicense"
+
+import os 
+
+Current_language = os.getenv("LANG","en_US")[:5]
+msg = "hello, world!"
+
+if current_language == "pt_BR":
+    msg = "olá, Mundo!"
+	
+elif current_language == "it_IT":	
+	msg = "Ciao, Mondo!"
+	
+print (msg)
