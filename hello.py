eca1550899f5050875b5dc28153d75e455b0c81c
Francisco Cavalcante <diegocavalcante.ti@gmail.com>
2024-07-17 20:48:54 +0000

    refeito o codigo e conferido alguns pontos

--- hello.py
+++ hello.py
@@ -1,32 +1,39 @@
+#!/usr/bin/env python3
 """Hello World Multi Linguas.
 
-dependendo da lingua configurada no ambiente o programa exibe a mensagem
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
 correspondente.
 
-como usar :
+Como usar:
 
-tenha a variável LANG devidamente configurada ex:
+Tenha a variável LANG devidamente configurada ex:
 
     export LANG=pt_BR
 
-execução:
+Execução:
+
     python3 hello.py
-	ou
-	./hello.py
+    ou
+    ./hello.py
 """
-__version__ ="0.0.1"	
-__author__ = "Francisco Cavalcante"
-__licence__ = "unlicense"
+__version__ = "0.0.1"
+__author__ = "Bruno Rocha"
+__license__ = "Unlicense"
 
-import os 
+import os
 
-Current_language = os.getenv("LANG","en_US")[:5]
-msg = "hello, world!"
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+
+# Aqui aplicamos a condicional
 
 if current_language == "pt_BR":
-    msg = "olá, Mundo!"
-	
-elif current_language == "it_IT":	
+	msg = "Olá, Mundo!"
+elif current_language == "it_IT":
 	msg = "Ciao, Mondo!"
-	
-print (msg)
+elif current_language == "fr_FR":
+	msg = "Bonjour, Monde!"
+
+print(msg)
